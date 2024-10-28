1b420fc3dea329aabd01c54c359d6bee761fc0dc
emzethpro <emzethpro@gmail.com>
2024-10-28 19:56:05 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RTA Dubai API'
+copyright = '2024, Patrick'
+author = 'Patrick'
 
 release = '0.1'
 version = '0.1.0'
